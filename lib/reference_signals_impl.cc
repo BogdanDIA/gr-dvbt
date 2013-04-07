6de9c80ad91a9938712ac95eee19fe934e47d78e
Bogdan Diaconescu <b_diaconescu@yahoo.com>
2013-04-07 14:37:01 +0300

    Add changes for new blocks

--- lib/reference_signals_impl.cc
+++ lib/reference_signals_impl.cc
@@ -27,10 +27,18 @@
 #include <complex>
 #include <stdio.h>
 
+#define TPS_DEBUG 1
+
+#ifdef TPS_DEBUG
+#define PRINTF(a...) printf(a)
+#else
+#define PRINTF(a...)
+#endif
+
 namespace gr {
   namespace dvbt {
 
-   //Number of symbols in a frame
+    //Number of symbols in a frame
     const int pilot_gen::d_symbols_per_frame = SYMBOLS_PER_FRAME;
     //Number of frames in a superframe
     const int pilot_gen::d_frames_per_superframe = FRAMES_PER_SUPERFRAME;
@@ -104,6 +112,15 @@ namespace gr {
     5681, 5707, 5800, 5902, 6013, 6185, 6331, 6374
     };
 
+    // TPS sync sequence for odd and even frames
+    const int pilot_gen::d_tps_sync_size = 16; // TODO
+    const int pilot_gen::d_tps_sync_even[d_tps_sync_size] = {
+      0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0
+    };
+    const int pilot_gen::d_tps_sync_odd[d_tps_sync_size] = {
+      1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1
+    };
+
     /*
      * Constructor of class
      */
@@ -112,6 +129,7 @@ namespace gr {
                                         d_cpilot_index(0),
                                         d_tpilot_index(0),
                                         d_symbol_index(0),
+                                        d_symbol_index_known(0),
                                         d_frame_index(0),
                                         d_superframe_index(0)
     {
@@ -153,6 +171,22 @@ namespace gr {
         return;
       }
 
+      // allocate buffer for scattered pilots
+      d_spilot_carriers_val = new gr_complex[d_Kmax - d_Kmin + 1];
+      if (d_spilot_carriers_val == NULL)
+      {
+        std::cout << "error allocating d_tps_carriers_val" << std::endl;
+        return;
+      }
+
+      // allocate buffer for channel gains (for each useful carrier)
+      d_channel_gain = new gr_complex[d_Kmax - d_Kmin + 1];
+      if (d_channel_gain == NULL)
+      {
+        std::cout << "error allocating d_tps_carriers_val" << std::endl;
+        return;
+      }
+
       // allocate buffer for first tps symbol constellation
       d_tps_carriers_val = new gr_complex[d_tps_carriers_size];
       if (d_tps_carriers_val == NULL)
@@ -164,6 +198,41 @@ namespace gr {
       // allocate tps data buffer
       d_tps_data = new unsigned char[d_symbols_per_frame];
       if (d_tps_data == NULL)
+      {
+        std::cout << "error allocating d_tps_data" << std::endl;
+        return;
+      }
+
+      d_prev_tps_symbol = new gr_complex[d_tps_carriers_size];
+      if (d_prev_tps_symbol == NULL)
+      {
+        std::cout << "error allocating d_tps_data" << std::endl;
+        return;
+      }
+      memset(d_prev_tps_symbol, 0, d_tps_carriers_size * sizeof(gr_complex));
+
+      d_tps_symbol = new gr_complex[d_tps_carriers_size];
+      if (d_tps_symbol == NULL)
+      {
+        std::cout << "error allocating d_tps_data" << std::endl;
+        return;
+      }
+      memset(d_tps_symbol, 0, d_tps_carriers_size * sizeof(gr_complex));
+
+      // Init receive TPS data vector
+      for (int i = 0; i < d_symbols_per_frame; i++)
+        d_rcv_tps_data.push_back(0);
+
+      // Init TPS sync sequence
+      for (int i = 0; i < d_tps_sync_size; i++)
+      {
+        d_tps_sync_evenv.push_back(d_tps_sync_even[i]);
+        d_tps_sync_oddv.push_back(d_tps_sync_odd[i]);
+      }
+
+      // Allocate buffer for payload carriers
+      d_payload_carriers = new int[d_Kmax - d_Kmin + 1];
+      if (d_payload_carriers == NULL)
       {
         std::cout << "error allocating d_tps_data" << std::endl;
         return;
@@ -185,6 +254,11 @@ namespace gr {
       delete [] d_wk;
       delete [] d_tps_carriers_val;
       delete [] d_tps_data;
+      delete [] d_prev_tps_symbol;
+      delete [] d_tps_symbol;
+      delete [] d_spilot_carriers_val;
+      delete [] d_channel_gain;
+      delete [] d_payload_carriers;
     }
 
     /*
@@ -243,6 +317,49 @@ namespace gr {
         d_tps_data[i + 54] = 0x1 & (reg_bch >> i); 
     }
 
+    int
+    pilot_gen::verify_bch_code(std::deque<char> data)
+    {
+      int ret = 0;
+
+      //TODO
+      //DO other way: if (feedback == 1) reg = reg ^ polymomial
+      //else nothing
+
+      //(n, k) = (127, 113) = (60+67, 60+53)
+      unsigned int reg_bch = 0;
+      unsigned char data_in[113];
+
+      //fill in 60 zeros
+      memset(&data_in[0], 0, 60);
+      //fill in TPS data - start bit not included
+      //memcpy(&data_in[60], &data[1], 53);
+      for (int i = 0; i < 53; i++)
+	data_in[60 + i] = data[1 + i]; 
+
+      //X^14+X^9+X^8+X^6+X^5+X^4+X^2+X+1
+      for (int i = 0; i < 113; i++)
+      {
+        int feedback = 0x1 & (data_in[i] ^ reg_bch);
+        reg_bch  = reg_bch >> 1;
+        reg_bch |= feedback << 13;
+        reg_bch = reg_bch \
+            ^ (feedback << 12) ^ (feedback << 11) \
+            ^ (feedback << 9) ^ (feedback << 8) \
+            ^ (feedback << 7) ^ (feedback << 5) \
+            ^ (feedback << 4);
+      }
+
+      for (int i = 0; i < 14; i++)
+        if (data[i + 54] != (0x1 & (reg_bch >> i)))
+        {
+          ret = -1;
+          break;
+        }
+
+      return ret;
+    }
+
     void
     pilot_gen::set_symbol_index(int sindex)
     {
@@ -272,7 +389,10 @@ namespace gr {
     pilot_gen::reset_pilot_generator()
     {
       d_spilot_index = 0; d_cpilot_index = 0; d_tpilot_index = 0;
+      d_payload_index = 0;
       d_symbol_index = 0; d_frame_index = 0; d_superframe_index = 0;
+      d_symbol_index_known = 0;
+      d_equalizer_ready = 0;
     }
 
     /*
@@ -281,7 +401,7 @@ namespace gr {
     int
     pilot_gen::get_current_spilot() const
     {
-      //TODO - can be optimised for same symbol_index
+      //TODO - can be optimized for same symbol_index
       return (d_Kmin + 3 * (d_symbol_index % 4) + 12 * d_spilot_index);
     }
 
@@ -293,9 +413,55 @@ namespace gr {
     }
 
     void
+    pilot_gen::set_spilot_value(int spilot, gr_complex val)
+    {
+      d_spilot_carriers_val[spilot] =val;
+    }
+
+    void
+    pilot_gen::set_channel_gain(int spilot, gr_complex val)
+    {
+      // Gain gval=rxval/txval
+      d_channel_gain[spilot] =val / gr_complex((4 * 2 * (0.5 - d_wk[spilot]) / 3), 0);
+    }
+    void
     pilot_gen::advance_spilot()
     {
       d_spilot_index = (++d_spilot_index) % d_spilot_carriers_size;
+    }
+
+    void
+    pilot_gen::process_spilot_data()
+    {
+      // This is channel estimator
+      // Interpolate the gain between carriers to obtain
+      // gain for non pilot carriers - we use linear interpolation
+      // Suppose we have g[k] and g[k+3] then
+      // g[k+1]=(2/3)v[k]+(1/3)v[k+3]
+      // g[k+2]=(1/3)v[k]+(2/3)v[k+3]
+
+      if (!d_symbol_index_known)
+	return;
+
+      // Wait for at least 4 symbols to have an estimation on each third carrier
+      if (d_symbol_index < 4)
+	return;
+
+      // This is only for debug
+      for (int i = 0; i < (d_Kmax - d_Kmin + 1); i++)
+	PRINTF("d_channel_gain[%i]: re: %f, img: %f\n", i, d_channel_gain[i].real(), d_channel_gain[i].imag());
+
+      // Suppose we have a pilot each third carrier.
+      for (int i = 0; i < (d_Kmax - d_Kmin + 1 - 3); i += 3)
+      {
+	d_channel_gain[i + 1] = 
+		(gr_complex(2, 0) * d_channel_gain[i] + d_channel_gain[i + 3]) / gr_complex(3, 0);
+	d_channel_gain[i + 2] = 
+		(d_channel_gain[i] + gr_complex(2, 0) * d_channel_gain[i + 3]) / gr_complex(3, 0);
+      }
+
+      // Signal that equalizer is ready
+      d_equalizer_ready = 1;
     }
 
     /*
@@ -320,8 +486,14 @@ namespace gr {
       d_cpilot_index = (++d_cpilot_index) % d_cpilot_carriers_size;
     }
 
+    void
+    pilot_gen::process_cpilot_data()
+    {
+
+    }
+
     /*
-     * Init tps sequence, return values for firs position
+     * Init tps sequence, return values for first position
      * If first symbol then init tps DBPSK data
      */
     int
@@ -417,6 +589,129 @@ namespace gr {
     }
 
     void
+    pilot_gen::process_tps_data()
+    {
+      //Look for TPS data only - demodulate DBPSK
+      //
+      // Compare previous symbol with current one to obtain bit values
+      // Use majority voting for decission
+      int tps_majority_zero = 0;
+
+      for (int k = 0; k < d_tps_carriers_size; k++)
+      {
+        if (!d_symbol_index_known || (d_symbol_index != 0))
+        {
+          if (d_prev_tps_symbol[k].real() * d_tps_symbol[k].real() >= 0.0)
+            tps_majority_zero++;
+          else
+            tps_majority_zero--;
+        }
+        else
+          PRINTF("S0[k]: k=%i, real: %f, imag: %f\n", k, d_tps_symbol[k].real(), d_tps_symbol[k].imag());
+
+        d_prev_tps_symbol[k] = d_tps_symbol[k];
+      }
+
+      // Insert obtained TPS bit into FIFO
+
+      // Take out the front entry first
+      d_rcv_tps_data.pop_front();
+
+      // Add data at tail
+      if (!d_symbol_index_known || (d_symbol_index != 0))
+      {
+        if (tps_majority_zero >= 0)
+          d_rcv_tps_data.push_back(0);
+        else
+          d_rcv_tps_data.push_back(1);
+      }
+      else
+	d_rcv_tps_data.push_back(0);
+
+      // Increment symbol index
+      if (d_symbol_index_known)
+      	d_symbol_index++;
+
+      // Match synchronization signatures
+      if (std::equal(d_rcv_tps_data.begin() + 1, d_rcv_tps_data.begin() + d_tps_sync_evenv.size(), d_tps_sync_evenv.begin()))
+      {
+          // Verify parity for TPS data
+          if (!verify_bch_code(d_rcv_tps_data))
+            PRINTF("TPS data parity OK for frame 0 or 2\n");
+          else
+            PRINTF("TPS data parity Not OK for frame 0 or 2\n");
+
+	  for (int i = 0; i < 68; i++)
+	 	PRINTF("%i", d_rcv_tps_data[i]);
+	  PRINTF("\n");
+
+	  // Clear up FIFO
+          for (int i = 0; i < 68; i++)
+            d_rcv_tps_data[i] = 0;
+
+          d_symbol_index_known = 1;
+          d_symbol_index = 0;
+      }
+      else if (std::equal(d_rcv_tps_data.begin() + 1, d_rcv_tps_data.begin() + d_tps_sync_oddv.size(), d_tps_sync_oddv.begin()))
+      {
+          // Verify parity for TPS data
+          if (!verify_bch_code(d_rcv_tps_data))
+            PRINTF("TPS data parity OK for frame 1 or 3\n");
+          else
+            PRINTF("TPS data parity Not OK for frame 1 or 3\n");
+
+	  for (int i = 0; i < 68; i++)
+              PRINTF("%i", d_rcv_tps_data[i]);
+	  PRINTF("\n");
+
+	  // Clear up FIFO
+          for (int i = 0; i < 68; i++)
+            d_rcv_tps_data[i] = 0;
+
+          d_symbol_index_known = 1;
+          d_symbol_index = 0;
+      }     
+    }
+
+    int
+    pilot_gen::get_current_payload()
+    {
+      return d_payload_carriers[d_payload_index];
+    }
+
+    void
+    pilot_gen::set_payload_carrier(int k)
+    {
+      d_payload_carriers[d_payload_index] = k;
+    }
+
+    void
+    pilot_gen::advance_payload()
+    {
+      d_payload_index++;
+    }
+
+    void
+    pilot_gen::process_payload_data(const gr_complex *in, gr_complex *out)
+    {
+      if (!d_symbol_index_known)
+	return;
+
+      if (d_equalizer_ready)
+      {
+      	// Equalize payload data according to channel estimator
+      	for (int k = 0; k < d_payload_index; k++)
+	  out[d_payload_carriers[k] + 172] = in[d_payload_carriers[k] + 172] / d_channel_gain[d_payload_carriers[k]];
+      }
+      else
+      {
+	// If equ not ready, copy payload from input to output as is
+	for (int k = 0; k < d_payload_index; k++)
+	  out[d_payload_carriers[k] + 172] = in[d_payload_carriers[k] + 172];
+      }
+    }
+
+    void
     pilot_gen::update_output(const gr_complex *in, gr_complex *out)
     {
       int is_payload = 1;
@@ -470,6 +765,68 @@ namespace gr {
       }
     }
 
+    void
+    pilot_gen::parse_input(const gr_complex *in, const unsigned char *trigger, gr_complex *out)
+    {
+      int is_payload = 1;
+
+      // If this block does not have a trigger then we just exit
+      if (trigger[0] != 1)
+        return;
+
+      //reset indexes
+      d_spilot_index = 0; d_cpilot_index = 0; d_tpilot_index = 0; d_payload_index = 0;
+
+      //process one block - one symbol
+      for (int k = 0; k < (d_Kmax - d_Kmin + 1); k++)
+      {
+        is_payload = 1;
+
+        if (d_symbol_index_known && (k == get_current_spilot()))
+        {
+          // Keep data for channel estimator
+          set_channel_gain(k, in[k + 172]);
+          advance_spilot(); 
+          is_payload = 0;
+        }
+
+        if (k == get_current_cpilot())
+        {
+          advance_cpilot();
+          is_payload = 0;
+        }
+        
+        if (k == get_current_tpilot())
+        {
+          d_tps_symbol[d_tpilot_index] = in[k + 172]; //TODO - obtain 172 from fft_len and used_carriers
+          PRINTF("symbol_index: %i, S[k]: k=%i, real: %f, imag: %f\n", \
+            d_symbol_index, d_tpilot_index, d_tps_symbol[d_tpilot_index].real(), d_tps_symbol[d_tpilot_index].imag());
+          advance_tpilot();
+          is_payload = 0;
+        } 
+
+        if (is_payload)
+        {
+          // TODO - do this online using the equ from previous symbol
+          set_payload_carrier(k);
+          advance_payload();
+        }
+      }
+
+      // Process spilot data
+      // This is channel estimation
+      process_spilot_data();
+
+      // Process cpilot data
+      process_cpilot_data();
+
+      // Process TPS data
+      process_tps_data();
+
+      // Process payload data 
+      process_payload_data(in, out);
+    }
+
     reference_signals::sptr
     reference_signals::make(int itemsize, int ninput, int noutput, \
         dvbt_constellation_t constellation, dvbt_hierarchy_t hierarchy, \
@@ -495,7 +852,7 @@ namespace gr {
 		      config(constellation, hierarchy, code_rate_HP, code_rate_LP, \
               guard_interval, transmission_mode, include_cell_id, cell_id),
           d_ninput(ninput), d_noutput(noutput),
-		      d_pg(config)
+	  d_pg(config)
     {
       //
     }

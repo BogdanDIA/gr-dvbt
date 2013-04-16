38d819d2d88a7afe13e69f0c9d1a71404b2e3191
Bogdan Diaconescu <b_diaconescu@yahoo.com>
2013-04-16 20:09:16 +0300

    add capture.sh

---
+++ grc/capture.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#./benchmark_rx.py -f 570e6 -m qam16 --fft-length=2048 --occupied-tones=1512 --cp-length=64 --snr=50 -W 10e6
+
+sudo uhd_rx_cfile -f 570e6 --gain=0  --samp-rate=10e6 ./captureBB.bin
+
+#./benchmark_rx.py -m qam16 --fft-length=2048 --occupied-tones=1512 --cp-length=64 --snr=40 -W 10e6 --from-file=./captureBB_benchmark.bin

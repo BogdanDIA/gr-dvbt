054d7aa343604e8ad1a1b872818ee9153684e282
Bogdan Diaconescu <b_diaconescu@yahoo.com>
2013-05-16 19:23:52 +0300

    Added 8k grc

--- grc/capture.sh
+++ grc/capture.sh
@@ -2,6 +2,8 @@
 
 #./benchmark_rx.py -f 570e6 -m qam16 --fft-length=2048 --occupied-tones=1512 --cp-length=64 --snr=50 -W 10e6
 
-sudo uhd_rx_cfile -f 570e6 --gain=0  --samp-rate=10e6 ./captureBB.bin
+sudo uhd_rx_cfile -f 546e6 --gain=25  --samp-rate=10e6 ./captureBB.bin
+
+#sudo uhd_rx_cfile -f 570e6 --gain=0  --samp-rate=10e6 ./captureBB.bin
 
 #./benchmark_rx.py -m qam16 --fft-length=2048 --occupied-tones=1512 --cp-length=64 --snr=40 -W 10e6 --from-file=./captureBB_benchmark.bin

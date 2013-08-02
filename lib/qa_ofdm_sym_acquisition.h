8cd2c8f95581da483e889de3afd270a01f114618
Bogdan Diaconescu <b_diaconescu@yahoo.com>
2013-08-02 12:24:42 +0300

    Include name and e-mail address

--- lib/qa_ofdm_sym_acquisition.h
+++ lib/qa_ofdm_sym_acquisition.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2013 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2013 <Bogdan Diaconescu, yo3iiu@yo3iiu.ro>.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

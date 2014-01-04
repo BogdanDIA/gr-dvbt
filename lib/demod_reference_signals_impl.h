3e3fab59387a3be232847126b7459c835bef977b
Bogdan Diaconescu <b_diaconescu@yahoo.com>
2014-01-04 17:17:58 +0200

    Fixed indentation for demod_reference_signals_impl.h

--- lib/demod_reference_signals_impl.h
+++ lib/demod_reference_signals_impl.h
@@ -33,29 +33,29 @@ namespace gr {
       // configuration object for this class
       const dvbt_config config;
 
-      private:
-	// Pilot Generator object
-    	pilot_gen d_pg;
+    private:
+      // Pilot Generator object
+      pilot_gen d_pg;
 
-	//In and Out data length
-	int d_ninput;
-	int d_noutput;
+      //In and Out data length
+      int d_ninput;
+      int d_noutput;
 
     public:
       demod_reference_signals_impl(int itemsize, int ninput, int noutput, \
-	      dvbt_constellation_t constellation, dvbt_hierarchy_t hierarchy, \
-	      dvbt_code_rate_t code_rate_HP, dvbt_code_rate_t code_rate_LP, \
-	      dvbt_guard_interval_t guard_interval, \
-	      dvbt_transmission_mode_t transmission_mode = gr::dvbt::T2k, int include_cell_id = 0, int cell_id = 0);
+        dvbt_constellation_t constellation, dvbt_hierarchy_t hierarchy, \
+        dvbt_code_rate_t code_rate_HP, dvbt_code_rate_t code_rate_LP, \
+        dvbt_guard_interval_t guard_interval, \
+        dvbt_transmission_mode_t transmission_mode = gr::dvbt::T2k, int include_cell_id = 0, int cell_id = 0);
       ~demod_reference_signals_impl();
 
       void forecast (int noutput_items, gr_vector_int &ninput_items_required);
 
       // Where all the action really happens
       int general_work(int noutput_items,
-		       gr_vector_int &ninput_items,
-		       gr_vector_const_void_star &input_items,
-		       gr_vector_void_star &output_items);
+           gr_vector_int &ninput_items,
+           gr_vector_const_void_star &input_items,
+           gr_vector_void_star &output_items);
     };
 
   } // namespace dvbt

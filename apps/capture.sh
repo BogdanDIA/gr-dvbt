84ceaa154c1386ed71d6ac9d973c12e40ccf96e6
Bogdan Diaconescu <b_diaconescu@yahoo.com>
2014-01-04 20:58:55 +0200

    Cleaned-up grc files.

--- apps/capture.sh
+++ apps/capture.sh
@@ -2,7 +2,7 @@
 
 #./benchmark_rx.py -f 570e6 -m qam16 --fft-length=2048 --occupied-tones=1512 --cp-length=64 --snr=50 -W 10e6
 
-sudo uhd_rx_cfile -a addr=192.168.10.2 -f 570000000 --gain=19  --samp-rate=10e6 ./captureBB_2.bin
+sudo uhd_rx_cfile -a addr=192.168.10.2 -f 1280022000 --gain=19  --samp-rate=10e6 ./captureBB_3.bin
 
 #sudo uhd_rx_cfile -f 570e6 --gain=0  --samp-rate=10e6 ./captureBB.bin
 
